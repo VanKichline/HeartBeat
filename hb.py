d064a3deabfb086a898f294fdef51af401fbb1d8
Van Kichline <van@kichline.com>
2015-09-19 22:53:10 +0000

    added logging to /var/tmp/heartbeat.log

--- hb.py
+++ hb.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python
-import time, httplib
+import time, httplib,logging
 
 apiHost = 'kichline-heartbeat.azurewebsites.net'
 apiPort = 80
 apiPath = '/api/heartbeat'
-apiQuery= 'group=Kichline.Kirkland&device=RPi2&service=uptime&status='
+apiQuery= 'group=Kichline.Kirkland&device=BB1&service=uptime&status='
+logLevel = logging.INFO
 
-startTime = time.time()
+def setup_logger():
+  global logLevel
+  logger = logging.getLogger('heartbeat')
+  hdlr = logging.FileHandler('/var/tmp/heartbeat.log')
+  formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+  hdlr.setFormatter(formatter)
+  logger.addHandler(hdlr)
+  logger.setLevel(logLevel)
+  return logger
 
 def hb(stat):
   conn = httplib.HTTPConnection(apiHost, apiPort)
   conn.connect()
   conn.set_debuglevel(0)
-  request = conn.putrequest('POST', apiPath+'?'+apiQuery+stat)
+  msg = apiPath+'?'+apiQuery+stat
+  request = conn.putrequest('POST', msg)
   headers = {}
   headers['Content-Length'] = "0"
   headers['Accept'] = '*/*'
@@ -21,9 +31,12 @@ def hb(stat):
   conn.endheaders()
   
   conn.send('')
+  logger.info(msg)
   resp = conn.getresponse()
   conn.close()
 
+startTime = time.time()
+logger = setup_logger()
 while True:
   interval = int(time.time() - startTime)
   hb(str(interval))
